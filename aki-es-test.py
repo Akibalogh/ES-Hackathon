7f146982c55eed1aacdc673dcb734d8ccd92758c
Aki Balogh <akibalogh@gmail.com>
2013-09-28 10:10:20 -0700

    pos and neg sentiment

--- aki-es-test.py
+++ aki-es-test.py
@@ -8,16 +8,10 @@ if __name__ == "__main__":
 	
 	results = es.search(index="twitter", body = 
 	{ 
-		"size": "7102208", 
+		"size": "7364415", 
 		"query": 
 			{"match": 
 				{"status.text": "obama"} 
-			},
-		"range":
-			{"_score":
-				{
-				"from": 4
-				}
 			}
 	}
 	)
@@ -25,6 +19,56 @@ if __name__ == "__main__":
 	#pprint(results)
 
 	print "max: ", results['hits']['max_score']
+	reslist = []
+	negsentimentlist = {}
+	possentimentlist = {}
 
 	for result in results['hits']['hits']:
-		print result['_score']
+		reslist.append(result['_source']['text'])
+		#res = result['_score']
+		#if (res > 4):
+		#	print res
+
+	f = open('negative-words.txt', 'r')
+	for line in f.readlines():
+		if (";" in line):
+			continue
+
+		line = line.rstrip().lstrip()
+
+		try: 
+			for res in reslist:
+				if (line in res and line != ''):
+					if (line in negsentimentlist):
+						negsentimentlist[line] += 1
+					else:
+						negsentimentlist[line] = 1
+		except KeyError:
+			print "error: ", line, " | ", res
+
+		except UnicodeDecodeError:
+			continue
+
+        f = open('positive-words.txt', 'r')
+        for line in f.readlines():
+                if (";" in line):
+                        continue
+
+                line = line.rstrip().lstrip()
+
+                try:
+                        for res in reslist:
+                                if (line in res and line != ''):
+                                        if (line in possentimentlist):
+                                                possentimentlist[line] += 1
+                                        else:
+                                                possentimentlist[line] = 1
+                except KeyError:
+                        print "error: ", line, " | ", res
+
+                except UnicodeDecodeError:
+                        continue
+
+
+	pprint(negsentimentlist)
+	pprint(possentimentlist)
